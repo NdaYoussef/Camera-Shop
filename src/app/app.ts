3115794483bbfa923eba92bc4828f90235ed9f96
NdaMousa <ndayou99@gmail.com>
2026-05-02 15:38:16 +0300

    edit app add header nav links

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './models/models';
+import { HeaderComponent } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ProductsComponent],
+  imports: [RouterOutlet, ProductsComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
