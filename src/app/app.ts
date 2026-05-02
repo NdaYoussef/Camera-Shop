395b9d8349943540cd6f19bb2da0f08e05a4cbfa
NdaMousa <ndayou99@gmail.com>
2026-05-02 15:43:21 +0300

    add home configs

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductsComponent } from './models/models';
 import { HeaderComponent } from "./header/header";
+import { HomeComponent } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductsComponent, HeaderComponent],
+  imports: [RouterOutlet, ProductsComponent, HeaderComponent,HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
