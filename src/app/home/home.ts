89cfdec407cf3e95e7a05175a0fe23a2fe367b27
NdaMousa <ndayou99@gmail.com>
2026-05-03 20:17:35 +0300

    edit home add promise section ts file

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent {}
+export class HomeComponent {
+  features = [
+    { num: '01', label: 'Universal Compatibility'       },
+    { num: '02', label: 'Real-Time Eye AF Technology'   },
+    { num: '03', label: 'High-Speed BIONZ XR Processing'},
+  ];
+}
